50606ff8b67add1f8d5cc4cad2f3d3c3dc38d003
time <liujun19950425@yeah.net>
2021-02-01 10:33:29 +0800

    mysql配置

--- Time_mall/Time_mall/settings/dev.py
+++ Time_mall/Time_mall/settings/dev.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'Time_mall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Time_mall',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'Time',
+        'PASSWORD': 'liujun'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
