218b6810760e6680fb9a334f81fa113a7aa0b55e
time <liujun19950425@yeah.net>
2021-02-01 13:29:45 +0800

    静态文件路径

--- Time_mall/Time_mall/settings/dev.py
+++ Time_mall/Time_mall/settings/dev.py
@@ -149,6 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#静态文件路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #配置工程日志
 LOGGING = {
     'version': 1,
