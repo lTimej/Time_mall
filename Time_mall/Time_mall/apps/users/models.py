9ccb0d1eac52333da08c2aed2e02be1ebab32605
liujun19950425@yeah.net <liujun19950425@yeah.net>
2021-02-02 10:22:56 +0800

    定义用户模型类

---
+++ Time_mall/Time_mall/apps/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=11,unique=True,verbose_name='手机号')
+    class Meta:
+        db_table = 'tb_users'#数据库表名
+        verbose_name = '用户'# 在admin站点中显示的名称
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        """定义每个数据对象的显示信息"""
+        return self.username
