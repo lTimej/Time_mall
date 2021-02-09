2e9a22ab80f1dc7707d207d04340b6d88e973a13
liujun19950425@yeah.net <liujun19950425@yeah.net>
2021-02-09 14:49:32 +0800

    将省市区缓存入redis内

--- Time_mall/Time_mall/apps/areas/views.py
+++ Time_mall/Time_mall/apps/areas/views.py
@@ -3,6 +3,7 @@ import logging
 from django import http
 from django.shortcuts import render
 from django.views import View
+from django.core.cache import cache
 
 from areas.models import Areas
 
@@ -18,28 +19,36 @@ class AreasView(View):
         #重构省市区结构
         areas_id = request.GET.get('areas_id')
         if not areas_id:
-            try:#省为一级，没有父级id
-                #获取省份  parent_id为null
-                provinces = Areas.objects.filter(parent_id__isnull=True).values('id', 'name')
-                province_list = []
-                #序列化，保存到列表内
-                for province in provinces:
-                    province_list.append(province)
-                #返回前端
-                return http.JsonResponse({"code":"0","errmsg":"ok","province_list":province_list})
-            except Exception as e:
-                logger.error(e)
-                return http.HttpResponseForbidden("获取省份数据失败")
+            #将省份进行缓存到redis 0号库
+            province_list = cache.get("province_list")
+            #不存在就加入缓存中
+            if not province_list:
+                try:#省为一级，没有父级id
+                    #获取省份  parent_id为null
+                    provinces = Areas.objects.filter(parent_id__isnull=True).values('id', 'name')
+                    province_list = []
+                    #序列化，保存到列表内
+                    for province in provinces:
+                        province_list.append(province)
+                    cache.set("province_list",province_list,3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.HttpResponseForbidden("获取省份数据失败")
+                    # 返回前端
+            return http.JsonResponse({"code": "0", "errmsg": "ok", "province_list": province_list})
         else:
-            try:#根据响应第几级id获取下级管辖区
-                #获取当前选中的地区对象
-                province_obj = Areas.objects.get(id=areas_id)
-                #通过related_name查找所有的下级地区
-                sub_cities = province_obj.subs.all().values("id","name")
-                sub_city_list = []
-                for sub_city in sub_cities:
-                    sub_city_list.append(sub_city)
-                return http.JsonResponse({"code": "0", "errmsg": "ok", "sub_city_list": sub_city_list})
-            except Exception as e:
-                logger.error(e)
-                return http.HttpResponseForbidden("获取省份下级地区数据失败")
+            sub_city_list = cache.get("sub_city_list")
+            if not sub_city_list:
+                try:#根据响应第几级id获取下级管辖区
+                    #获取当前选中的地区对象
+                    province_obj = Areas.objects.get(id=areas_id)
+                    #通过related_name查找所有的下级地区
+                    sub_cities = province_obj.subs.all().values("id","name")
+                    sub_city_list = []
+                    for sub_city in sub_cities:
+                        sub_city_list.append(sub_city)
+                    cache.set("sub_city_list",sub_city_list,3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.HttpResponseForbidden("获取省份下级地区数据失败")
+            return http.JsonResponse({"code": "0", "errmsg": "ok", "sub_city_list": sub_city_list})
