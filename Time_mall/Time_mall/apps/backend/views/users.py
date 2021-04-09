d4daa88c617bb8f1c76721833f74e1840fa4fb32
time <liujun19950425@yeah.net>
2021-04-09 14:56:45 +0800

    解决跨域问题

--- Time_mall/Time_mall/apps/backend/views/users.py
+++ Time_mall/Time_mall/apps/backend/views/users.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 class backendLoginView(View):
-    def get(self,request):
+    def post(self,request):
+        json_data = request.body
+        print(json_data)
         return http.JsonResponse({"code":"ok"})
